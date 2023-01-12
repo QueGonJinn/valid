dd2ed224bb596070c848e6e6b66f3475e6866058
Yury Moliakow <molialow13@gmail.com>
2023-01-13 01:03:21 +0300

    feat: add new field

--- business/index.js
+++ business/index.js
@@ -35,6 +35,12 @@ const companyAddres = document.getElementById('seller_sellerCompany_address');
 const companyAbout = document.getElementById('seller_sellerCompany_activityDescription');
 const companySite = document.getElementById('seller_sellerCompany_siteAddress');
 const companyLink = document.getElementById('seller_sellerCompany_linkImportAdverts');
+const sellerAdvantagesFirst = document.getElementById('seller_advantages_advantage1');
+const sellerAdvantagesSecond = document.getElementById('seller_advantages_advantage2');
+const sellerAdvantagesThrid = document.getElementById('seller_advantages_advantage3');
+const sellerAdvantagesFour = document.getElementById('seller_advantages_advantage4');
+const sellerAdvantagesFives = document.getElementById('seller_advantages_advantage5');
+const coments = document.getElementById('seller_commentOrDescription');
 
 const parrentBlock = document.querySelectorAll('.input-outter');
 
@@ -44,6 +50,7 @@ parrentBlock.forEach((e) => {
 });
 
 const parrentBlockText = document.querySelectorAll('.delivery-text');
+const parrentBlockComments = document.querySelector('.textarea-bus-comment');
 
 let labelCompanyNameMinMax = document.createElement('span');
 parrentBlock[0].appendChild(labelCompanyNameMinMax);
@@ -73,6 +80,26 @@ let labelCompanyAboutMinMax = document.createElement('span');
 parrentBlock[6].appendChild(labelCompanyAboutMinMax);
 labelCompanyAboutMinMax.setAttribute('style', 'color: #2d4059;');
 
+let labelSellerAdvantagesFirst = document.createElement('span');
+parrentBlock[7].appendChild(labelSellerAdvantagesFirst);
+labelSellerAdvantagesFirst.setAttribute('style', 'color: #2d4059;');
+
+let labelSellerAdvantagesSecond = document.createElement('span');
+parrentBlock[8].appendChild(labelSellerAdvantagesSecond);
+labelSellerAdvantagesSecond.setAttribute('style', 'color: #2d4059;');
+
+let labelSellerAdvantagesThrid = document.createElement('span');
+parrentBlock[9].appendChild(labelSellerAdvantagesThrid);
+labelSellerAdvantagesThrid.setAttribute('style', 'color: #2d4059;');
+
+let labelSellerAdvantagesFour = document.createElement('span');
+parrentBlock[10].appendChild(labelSellerAdvantagesFour);
+labelSellerAdvantagesFour.setAttribute('style', 'color: #2d4059;');
+
+let labelSellerAdvantagesFives = document.createElement('span');
+parrentBlock[11].appendChild(labelSellerAdvantagesFives);
+labelSellerAdvantagesFives.setAttribute('style', 'color: #2d4059;');
+
 let labelCompanyDeliveryMinMax = document.createElement('span');
 parrentBlockText[0].appendChild(labelCompanyDeliveryMinMax);
 labelCompanyDeliveryMinMax.setAttribute('style', 'color: #2d4059;');
@@ -80,6 +107,10 @@ labelCompanyDeliveryMinMax.setAttribute('style', 'color: #2d4059;');
 let labelCompanyGarntieMinMax = document.createElement('span');
 parrentBlockText[1].appendChild(labelCompanyGarntieMinMax);
 labelCompanyGarntieMinMax.setAttribute('style', 'color: #2d4059;');
+
+let labelCommentsMinMax = document.createElement('span');
+parrentBlockComments.appendChild(labelCommentsMinMax);
+labelCommentsMinMax.setAttribute('style', 'color: #2d4059;');
 
 function validateCompanyName(name) {
 	return String(name).match(/^[a-zA-Zа-яА-ЯёЁ0-9 ",.&«»-]+$/);
@@ -146,7 +177,7 @@ companyName.addEventListener('input', (e) => {
 	}
 
 	e.target.value.length > 30
-		? ((labelCompanyNameMinMax.innerHTML = 'Достигнут лимит введенных символов'),
+		? ((labelCompanyNameMinMax.innerHTML = 'Достигнут лимит в 30 символов'),
 		  labelCompanyNameMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 30)))
 		: e.target.value.length < 30 && e.target.value.length > 10
@@ -191,7 +222,7 @@ companyName.addEventListener('input', (e) => {
 });
 companyName.addEventListener('focus', (e) => {
 	e.target.value.length > 30
-		? ((labelCompanyNameMinMax.innerHTML = 'Достигнут лимит введенных символов'),
+		? ((labelCompanyNameMinMax.innerHTML = 'Достигнут лимит в 30 символов'),
 		  labelCompanyNameMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 30)))
 		: e.target.value.length < 30 && e.target.value.length > 10
@@ -245,7 +276,7 @@ companyUNP.addEventListener('input', (e) => {
 		companyUNP.setAttribute('style', 'color: #ff7171;');
 	}
 	e.target.value.length > 20
-		? ((labelCompanyUNPMinMax.innerHTML = 'Достигнут лимит введенных символов'),
+		? ((labelCompanyUNPMinMax.innerHTML = 'Достигнут лимит в 20 символов'),
 		  labelCompanyUNPMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 20)))
 		: e.target.value.length < 20 && e.target.value.length > 5
@@ -257,7 +288,7 @@ companyUNP.addEventListener('input', (e) => {
 });
 companyUNP.addEventListener('focus', (e) => {
 	e.target.value.length > 20
-		? ((labelCompanyUNPMinMax.innerHTML = 'Достигнут лимит введенных символов'),
+		? ((labelCompanyUNPMinMax.innerHTML = 'Достигнут лимит в 20 символов'),
 		  labelCompanyUNPMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 20)))
 		: e.target.value.length < 20 && e.target.value.length > 5
@@ -311,7 +342,7 @@ companyCity.addEventListener('input', (e) => {
 		companyCity.setAttribute('style', 'color: #ff7171;');
 	}
 	e.target.value.length > 20
-		? ((labelCityUNPMinMax.innerHTML = 'Достигнут лимит введенных символов'),
+		? ((labelCityUNPMinMax.innerHTML = 'Достигнут лимит в 20 символов'),
 		  labelCityUNPMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 20)))
 		: e.target.value.length < 20 && e.target.value.length > 3
@@ -331,7 +362,7 @@ companyCity.addEventListener('input', (e) => {
 });
 companyCity.addEventListener('focus', (e) => {
 	e.target.value.length > 20
-		? ((labelCityUNPMinMax.innerHTML = 'Достигнут лимит введенных символов'),
+		? ((labelCityUNPMinMax.innerHTML = 'Достигнут лимит в 20 символов'),
 		  labelCityUNPMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 20)))
 		: e.target.value.length < 20 && e.target.value.length > 3
@@ -398,7 +429,7 @@ companyAddres.addEventListener('input', (e) => {
 	}
 
 	e.target.value.length > 45
-		? ((labelCompanyAddressMinMax.innerHTML = 'Достигнут лимит введенных символов'),
+		? ((labelCompanyAddressMinMax.innerHTML = 'Достигнут лимит в 45 символов'),
 		  labelCompanyAddressMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 45)))
 		: e.target.value.length < 45 && e.target.value.length > 10
@@ -432,7 +463,7 @@ companyAddres.addEventListener('input', (e) => {
 });
 companyAddres.addEventListener('focus', (e) => {
 	e.target.value.length > 45
-		? ((labelCompanyAddressMinMax.innerHTML = 'Достигнут лимит введенных символов'),
+		? ((labelCompanyAddressMinMax.innerHTML = 'Достигнут лимит в 45 символов'),
 		  labelCompanyAddressMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 45)))
 		: e.target.value.length < 45 && e.target.value.length > 10
@@ -445,7 +476,7 @@ companyAddres.addEventListener('focus', (e) => {
 companySite.addEventListener('input', (e) => {
 	let i = e.target.value.length - 1;
 	e.target.value.length > 80
-		? ((labelCompanySiteMinMax.innerHTML = 'Достигнут лимит введенных символов'),
+		? ((labelCompanySiteMinMax.innerHTML = 'Достигнут лимит в 80 символов'),
 		  labelCompanySiteMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 80)))
 		: e.target.value.length < 20 && e.target.value.length > 20
@@ -492,7 +523,7 @@ companySite.addEventListener('input', (e) => {
 });
 companySite.addEventListener('focus', (e) => {
 	e.target.value.length > 80
-		? ((labelCompanySiteMinMax.innerHTML = 'Достигнут лимит введенных символов'),
+		? ((labelCompanySiteMinMax.innerHTML = 'Достигнут лимит в 80 символов'),
 		  labelCompanySiteMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 80)))
 		: e.target.value.length < 20 && e.target.value.length > 20
@@ -505,7 +536,7 @@ companySite.addEventListener('focus', (e) => {
 companyLink.addEventListener('input', (e) => {
 	let i = e.target.value.length - 1;
 	e.target.value.length > 80
-		? ((labelCompanyLinkMinMax.innerHTML = 'Достигнут лимит введенных символов'),
+		? ((labelCompanyLinkMinMax.innerHTML = 'Достигнут лимит в 80 символов'),
 		  labelCompanyLinkMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 80)))
 		: e.target.value.length < 20 && e.target.value.length > 20
@@ -552,7 +583,7 @@ companyLink.addEventListener('input', (e) => {
 });
 companyLink.addEventListener('focus', (e) => {
 	e.target.value.length > 80
-		? ((labelCompanyLinkMinMax.innerHTML = 'Достигнут лимит введенных символов'),
+		? ((labelCompanyLinkMinMax.innerHTML = 'Достигнут лимит в 80 символов'),
 		  labelCompanyLinkMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 80)))
 		: e.target.value.length < 20 && e.target.value.length > 20
@@ -563,8 +594,12 @@ companyLink.addEventListener('focus', (e) => {
 });
 
 companyAbout.addEventListener('input', (e) => {
+	if (e.target.value.match(/^[a-zA-Zа-яА-ЯёЁ]/)) {
+		let s = e.target.value;
+		e.target.value = s[0].toUpperCase() + s.slice(1);
+	}
 	e.target.value.length > 200
-		? ((labelCompanyAboutMinMax.innerHTML = 'Достигнут лимит введенных символов в размере 200'),
+		? ((labelCompanyAboutMinMax.innerHTML = 'Достигнут лимит в 200 символов'),
 		  labelCompanyAboutMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 200)))
 		: ((labelCompanyAboutMinMax.innerHTML = 'Max колличество символов 200'),
@@ -572,7 +607,7 @@ companyAbout.addEventListener('input', (e) => {
 });
 companyAbout.addEventListener('focus', (e) => {
 	e.target.value.length > 200
-		? ((labelCompanyAboutMinMax.innerHTML = 'Достигнут лимит введенных символов в размере 200'),
+		? ((labelCompanyAboutMinMax.innerHTML = 'Достигнут лимит в 200 символов'),
 		  labelCompanyAboutMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 200)))
 		: ((labelCompanyAboutMinMax.innerHTML = 'Max колличество символов 200'),
@@ -580,8 +615,12 @@ companyAbout.addEventListener('focus', (e) => {
 });
 
 fieldDelivery.addEventListener('input', (e) => {
+	if (e.target.value.match(/^[a-zA-Zа-яА-ЯёЁ]/)) {
+		let s = e.target.value;
+		e.target.value = s[0].toUpperCase() + s.slice(1);
+	}
 	e.target.value.length > 150
-		? ((labelCompanyDeliveryMinMax.innerHTML = 'Достигнут лимит введенных символов в размере 150'),
+		? ((labelCompanyDeliveryMinMax.innerHTML = 'Достигнут лимит в 150 символов'),
 		  labelCompanyDeliveryMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 150)))
 		: ((labelCompanyDeliveryMinMax.innerHTML = 'Max колличество символов 150'),
@@ -589,7 +628,7 @@ fieldDelivery.addEventListener('input', (e) => {
 });
 fieldDelivery.addEventListener('focus', (e) => {
 	e.target.value.length > 150
-		? ((labelCompanyDeliveryMinMax.innerHTML = 'Достигнут лимит введенных символов в размере 150'),
+		? ((labelCompanyDeliveryMinMax.innerHTML = 'Достигнут лимит в 150 символов'),
 		  labelCompanyDeliveryMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 150)))
 		: ((labelCompanyDeliveryMinMax.innerHTML = 'Max колличество символов 150'),
@@ -597,8 +636,12 @@ fieldDelivery.addEventListener('focus', (e) => {
 });
 
 fielGuarantee.addEventListener('input', (e) => {
+	if (e.target.value.match(/^[a-zA-Zа-яА-ЯёЁ]/)) {
+		let s = e.target.value;
+		e.target.value = s[0].toUpperCase() + s.slice(1);
+	}
 	e.target.value.length > 150
-		? ((labelCompanyGarntieMinMax.innerHTML = 'Достигнут лимит введенных символов в размере 150'),
+		? ((labelCompanyGarntieMinMax.innerHTML = 'Достигнут лимит в 150 символов'),
 		  labelCompanyGarntieMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 150)))
 		: ((labelCompanyGarntieMinMax.innerHTML = 'Max колличество символов 150'),
@@ -606,9 +649,141 @@ fielGuarantee.addEventListener('input', (e) => {
 });
 fielGuarantee.addEventListener('focus', (e) => {
 	e.target.value.length > 150
-		? ((labelCompanyGarntieMinMax.innerHTML = 'Достигнут лимит введенных символов в размере 150'),
+		? ((labelCompanyGarntieMinMax.innerHTML = 'Достигнут лимит в 150 символов'),
 		  labelCompanyGarntieMinMax.setAttribute('style', 'color: #ff7171;'),
 		  (e.target.value = e.target.value.substr(0, 150)))
 		: ((labelCompanyGarntieMinMax.innerHTML = 'Max колличество символов 150'),
 		  labelCompanyGarntieMinMax.setAttribute('style', 'color: #2d4059;'));
+});
+
+sellerAdvantagesFirst.addEventListener('input', (e) => {
+	if (e.target.value.match(/^[a-zA-Zа-яА-ЯёЁ]/)) {
+		let s = e.target.value;
+		e.target.value = s[0].toUpperCase() + s.slice(1);
+	}
+	e.target.value.length > 250
+		? ((labelSellerAdvantagesFirst.innerHTML = 'Достигнут лимит в 250 символов'),
+		  labelSellerAdvantagesFirst.setAttribute('style', 'color: #ff7171;'),
+		  (e.target.value = e.target.value.substr(0, 250)))
+		: ((labelSellerAdvantagesFirst.innerHTML = 'Max колличество символов 250'),
+		  labelSellerAdvantagesFirst.setAttribute('style', 'color: #2d4059;'));
+});
+
+sellerAdvantagesFirst.addEventListener('focus', (e) => {
+	e.target.value.length > 250
+		? ((labelSellerAdvantagesFirst.innerHTML = 'Достигнут лимит в 250 символов'),
+		  labelSellerAdvantagesFirst.setAttribute('style', 'color: #ff7171;'),
+		  (e.target.value = e.target.value.substr(0, 250)))
+		: ((labelSellerAdvantagesFirst.innerHTML = 'Max колличество символов 250'),
+		  labelSellerAdvantagesFirst.setAttribute('style', 'color: #2d4059;'));
+});
+
+sellerAdvantagesSecond.addEventListener('input', (e) => {
+	if (e.target.value.match(/^[a-zA-Zа-яА-ЯёЁ]/)) {
+		let s = e.target.value;
+		e.target.value = s[0].toUpperCase() + s.slice(1);
+	}
+	e.target.value.length > 250
+		? ((labelSellerAdvantagesSecond.innerHTML = 'Достигнут лимит в 250 символов'),
+		  labelSellerAdvantagesSecond.setAttribute('style', 'color: #ff7171;'),
+		  (e.target.value = e.target.value.substr(0, 250)))
+		: ((labelSellerAdvantagesSecond.innerHTML = 'Max колличество символов 250'),
+		  labelSellerAdvantagesSecond.setAttribute('style', 'color: #2d4059;'));
+});
+
+sellerAdvantagesSecond.addEventListener('focus', (e) => {
+	e.target.value.length > 250
+		? ((labelSellerAdvantagesSecond.innerHTML = 'Достигнут лимит в 250 символов'),
+		  labelSellerAdvantagesSecond.setAttribute('style', 'color: #ff7171;'),
+		  (e.target.value = e.target.value.substr(0, 250)))
+		: ((labelSellerAdvantagesSecond.innerHTML = 'Max колличество символов 250'),
+		  labelSellerAdvantagesSecond.setAttribute('style', 'color: #2d4059;'));
+});
+
+sellerAdvantagesThrid.addEventListener('input', (e) => {
+	if (e.target.value.match(/^[a-zA-Zа-яА-ЯёЁ]/)) {
+		let s = e.target.value;
+		e.target.value = s[0].toUpperCase() + s.slice(1);
+	}
+	e.target.value.length > 250
+		? ((labelSellerAdvantagesThrid.innerHTML = 'Достигнут лимит в 250 символов'),
+		  labelSellerAdvantagesThrid.setAttribute('style', 'color: #ff7171;'),
+		  (e.target.value = e.target.value.substr(0, 250)))
+		: ((labelSellerAdvantagesThrid.innerHTML = 'Max колличество символов 250'),
+		  labelSellerAdvantagesThrid.setAttribute('style', 'color: #2d4059;'));
+});
+
+sellerAdvantagesThrid.addEventListener('focus', (e) => {
+	e.target.value.length > 250
+		? ((labelSellerAdvantagesThrid.innerHTML = 'Достигнут лимит в 250 символов'),
+		  labelSellerAdvantagesThrid.setAttribute('style', 'color: #ff7171;'),
+		  (e.target.value = e.target.value.substr(0, 250)))
+		: ((labelSellerAdvantagesThrid.innerHTML = 'Max колличество символов 250'),
+		  labelSellerAdvantagesThrid.setAttribute('style', 'color: #2d4059;'));
+});
+
+sellerAdvantagesFour.addEventListener('input', (e) => {
+	if (e.target.value.match(/^[a-zA-Zа-яА-ЯёЁ]/)) {
+		let s = e.target.value;
+		e.target.value = s[0].toUpperCase() + s.slice(1);
+	}
+	e.target.value.length > 250
+		? ((labelSellerAdvantagesFour.innerHTML = 'Достигнут лимит в 250 символов'),
+		  labelSellerAdvantagesFour.setAttribute('style', 'color: #ff7171;'),
+		  (e.target.value = e.target.value.substr(0, 250)))
+		: ((labelSellerAdvantagesFour.innerHTML = 'Max колличество символов 250'),
+		  labelSellerAdvantagesFour.setAttribute('style', 'color: #2d4059;'));
+});
+
+sellerAdvantagesFour.addEventListener('focus', (e) => {
+	e.target.value.length > 250
+		? ((labelSellerAdvantagesFour.innerHTML = 'Достигнут лимит в 250 символов'),
+		  labelSellerAdvantagesFour.setAttribute('style', 'color: #ff7171;'),
+		  (e.target.value = e.target.value.substr(0, 250)))
+		: ((labelSellerAdvantagesFour.innerHTML = 'Max колличество символов 250'),
+		  labelSellerAdvantagesFour.setAttribute('style', 'color: #2d4059;'));
+});
+
+sellerAdvantagesFives.addEventListener('input', (e) => {
+	if (e.target.value.match(/^[a-zA-Zа-яА-ЯёЁ]/)) {
+		let s = e.target.value;
+		e.target.value = s[0].toUpperCase() + s.slice(1);
+	}
+	e.target.value.length > 250
+		? ((labelSellerAdvantagesFives.innerHTML = 'Достигнут лимит в 250 символов'),
+		  labelSellerAdvantagesFives.setAttribute('style', 'color: #ff7171;'),
+		  (e.target.value = e.target.value.substr(0, 250)))
+		: ((labelSellerAdvantagesFives.innerHTML = 'Max колличество символов 250'),
+		  labelSellerAdvantagesFives.setAttribute('style', 'color: #2d4059;'));
+});
+
+sellerAdvantagesFives.addEventListener('focus', (e) => {
+	e.target.value.length > 250
+		? ((labelSellerAdvantagesFives.innerHTML = 'Достигнут лимит в 250 символов'),
+		  labelSellerAdvantagesSFilabelSellerAdvantagesFivessetAttribute('style', 'color: #ff7171;'),
+		  (e.target.value = e.target.value.substr(0, 250)))
+		: ((labelSellerAdvantagesFives.innerHTML = 'Max колличество символов 250'),
+		  labelSellerAdvantagesFives.setAttribute('style', 'color: #2d4059;'));
+});
+
+coments.addEventListener('input', (e) => {
+	if (e.target.value.match(/^[a-zA-Zа-яА-ЯёЁ]/)) {
+		let s = e.target.value;
+		e.target.value = s[0].toUpperCase() + s.slice(1);
+	}
+	e.target.value.length > 250
+		? ((labelCommentsMinMax.innerHTML = 'Достигнут лимит в 250 символов'),
+		  labelCommentsMinMax.setAttribute('style', 'color: #ff7171;'),
+		  (e.target.value = e.target.value.substr(0, 250)))
+		: ((labelCommentsMinMax.innerHTML = 'Max колличество символов 250'),
+		  labelCommentsMinMax.setAttribute('style', 'color: #2d4059;'));
+});
+
+coments.addEventListener('focus', (e) => {
+	e.target.value.length > 250
+		? ((labelCommentsMinMax.innerHTML = 'Достигнут лимит в 250 символов'),
+		  labelCommentsMinMax.setAttribute('style', 'color: #ff7171;'),
+		  (e.target.value = e.target.value.substr(0, 250)))
+		: ((labelCommentsMinMax.innerHTML = 'Max колличество символов 250'),
+		  labelCommentsMinMax.setAttribute('style', 'color: #2d4059;'));
 });
